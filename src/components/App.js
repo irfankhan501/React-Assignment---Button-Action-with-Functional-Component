7102692b8e91705187947988bbbd6cdb0381fa67
Irfan Khan <irfankhan501@outlook.com>
2020-11-12 00:31:14 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,17 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const { isParaShow, setState } = useState(false);
+  const [isParaShow, setPara] = useState(false);
 
   function handleClick() {
-    setState((isParaShow = true));
+    setPara(true);
   }
   return (
     <div id="main">
       <button id="click" onClick={handleClick}>
         click me!
       </button>
-      {this.state.isParaShow && (
+      {isParaShow && (
         <p id="para">
           Hello, I've learnt to use the full-stack evaluation tool. This makes
           me so happy
