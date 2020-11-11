1c8bf8ff2bd90efb9d9df188733683f7277fadcf
Irfan Khan <irfankhan501@outlook.com>
2020-11-11 21:43:38 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -2,22 +2,22 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [p, setState] = useState("");
-  const para = (
-    <p id="para">
-      Hello, I've learnt to use the full-stack evaluation tool. This makes me so
-      happy
-    </p>
-  );
+  const { isParaShow, setState } = useState(false);
+
   handleClick = () => {
-    setState(para);
+    setState((isParaShow = true));
   };
   return (
     <div id="main">
       <button id="click" onClick={handleClick}>
         click me!
       </button>
-      {p}
+      {this.state.isParaShow && (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
